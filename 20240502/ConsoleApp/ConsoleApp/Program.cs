f22e548d82ecfc0cbd13ef72734d676ed55e8fbc
Nats <prgrm.ntk5@gmail.com>
2024-05-02 23:35:05 +0900

    コンソールアプリとwindowアプリのベースを実装

---
+++ 20240502/ConsoleApp/ConsoleApp/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace ConsoleApp
+{
+    class Program
+    {
+        public int x { get; set; }
+        public int y { get; set; }
+
+        public override string ToString() => $"({x}, {y})";
+    }
+    class CosoleApp
+    {
+        static void Main(string[] args)
+        {
+            var p = new Program { x = 1, y = 2 };
+            Console.WriteLine(p);
+        }
+    }
+
+}
